b7413cb53d5b9ceeb327cb260d83f477055fc841
gitamarbun23-design <gitamarbun23@gmail.com>
2025-10-10 18:09:47 +0700

    Update Slip_gaji.py

--- Slip_gaji.py
+++ Slip_gaji.py
@@ -1,2 +1,16 @@
-print('isai')
+def garis():
+    print("=" * 45)
+
+def hitung_gaji(jabatan):
+    # Gaji pokok sesuai jabatan
+    if jabatan == "magang":
+        gaji_pokok = 1_500_000
+    elif jabatan == "karyawan":
+        gaji_pokok = 3_500_000
+    elif jabatan == "manajer":
+        gaji_pokok = 7_000_000
+    else:
+        print("Jabatan tidak dikenal!")
+        gaji_pokok = 0
+    return gaji_pokok
 
